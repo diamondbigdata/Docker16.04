58833e647193df8e651ac39fbb17410fb059e04f
diamondbigdata <diamondbigdata@gmail.com>
2016-07-23 22:55:39 +0800

    Delete initjava.sh

--- ubuntu-16.04-java8/initjava.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-export JRE_HOME=${JAVA_HOME}/jre
-export CLASS_PATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
-export PATH=${JAVA_HOME}/bin:$PATH
-ulimit -SHn 65535
